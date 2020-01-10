311d9632bd68fb113c140437038d96e114845507
Dave Lee <isho.lee@gmail.com>
2020-01-10 17:40:13 +0800

    feature was added again

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New feature added!
+          A new feature has been added!
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
